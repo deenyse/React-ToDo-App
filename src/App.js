8fa5b226d6c2242bb864c2c36c2b3fe05c2f1c05
denvery31 <denvery31@gmail.com>
2022-06-03 20:08:05 +0300

    easy counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [likes, setLikes] = useState(0);
+  console.log(likes);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>LikesCount is {likes}</h1>
+      <button onClick={() => setLikes(likes + 1)}>increment</button>
+      <button onClick={() => setLikes(likes - 1)}>decrement</button>
     </div>
   );
 }
